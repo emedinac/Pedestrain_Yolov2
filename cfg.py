12b025f5d4b02c0c41cbf8364f4d83c13b756258
edgarmedina1801 <edgarmedina1801@gmail.com>
2018-05-08 15:35:56 -0300

    support for pytorch 0.4

--- cfg.py
+++ cfg.py
@@ -1,5 +1,5 @@
 import torch
-from utils import convert2cpu
+from .utils import convert2cpu
 
 def parse_cfg(cfgfile):
     blocks = []
@@ -154,7 +154,13 @@ def load_conv(buf, start, conv_model):
     num_w = conv_model.weight.numel()
     num_b = conv_model.bias.numel()
     conv_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b]));   start = start + num_b
-    conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w
+    # conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w
+    conv_model.weight.data.copy_(torch.reshape(
+                                    torch.from_numpy(buf[start:start+num_w]),
+                                    (conv_model.weight.shape[0],
+                                    conv_model.weight.shape[1], 
+                                    conv_model.weight.shape[2],
+                                    conv_model.weight.shape[3]))); start=start + num_w
     return start
 
 def save_conv(fp, conv_model):
@@ -172,7 +178,13 @@ def load_conv_bn(buf, start, conv_model, bn_model):
     bn_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_b]));   start = start + num_b
     bn_model.running_mean.copy_(torch.from_numpy(buf[start:start+num_b]));  start = start + num_b
     bn_model.running_var.copy_(torch.from_numpy(buf[start:start+num_b]));   start = start + num_b
-    conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w 
+    # conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w 
+    conv_model.weight.data.copy_(torch.reshape(
+                                    torch.from_numpy(buf[start:start+num_w]),
+                                    (conv_model.weight.shape[0],
+                                    conv_model.weight.shape[1], 
+                                    conv_model.weight.shape[2],
+                                    conv_model.weight.shape[3]))); start=start + num_w
     return start
 
 def save_conv_bn(fp, conv_model, bn_model):
